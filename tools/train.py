c5d4f0e6cadf833ef229336a5af011a320cd691a
Shalin Shah <44682269+shalin-s@users.noreply.github.com>
2021-04-21 03:27:48 -0700

    Test Commit
    Fixed a one-letter typo. This commit is just to make sure that the git stuff is working properly.

--- tools/train.py
+++ tools/train.py
@@ -29,7 +29,7 @@ def parse_config():
     parser.add_argument('--ckpt', type=str, default=None, help='checkpoint to start from')
     parser.add_argument('--pretrained_model', type=str, default=None, help='pretrained_model')
     parser.add_argument('--launcher', choices=['none', 'pytorch', 'slurm'], default='none')
-    parser.add_argument('--tcp_port', type=int, default=18888, help='tcp port for distrbuted training')
+    parser.add_argument('--tcp_port', type=int, default=18888, help='tcp port for distributed training')
     parser.add_argument('--sync_bn', action='store_true', default=False, help='whether to use sync bn')
     parser.add_argument('--fix_random_seed', action='store_true', default=False, help='')
     parser.add_argument('--ckpt_save_interval', type=int, default=1, help='number of training epochs')
